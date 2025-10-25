b13194dddb816bb8a474089b6e11e9cdfceb911a
kmutyala01 <karthiksatya@gmail.com>
2025-10-25 17:36:42 -0500

    Implemented gpt wrapper

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
-﻿using LibGit2Sharp;
+﻿using DocGen;
+using LibGit2Sharp;
 using System.IO;
 
+
 // Define the repository URL and local path
-string repoUrl = "https://github.com/kmutyala01/ApartmentTracker";
+string repoUrl = args.Length == 0 ?  "https://github.com/kmutyala01/ApartmentTracker" : args[0];
+
 string localPath = "C:\\Users\\karth\\source\\repos\\DocGen\\ClonedRepos";
 
 // Clone the repository if it doesn't exist locally
@@ -15,13 +18,23 @@ else
 {
     Console.WriteLine("Repository already exists locally.");
 }
+Console.WriteLine(Response.getOpenAIResponse()); 
 
 // Enumerate and print all files in the cloned repository
-foreach (var file in Directory.EnumerateFiles(localPath, "*.*", SearchOption.AllDirectories))
-{
-    Console.WriteLine(file);
-}
+//foreach (var file in Directory.EnumerateFiles(localPath, "*.*", SearchOption.AllDirectories))
+//{
+//    if (!file.Contains($"{Path.DirectorySeparatorChar}.git{Path.DirectorySeparatorChar}"))
+//    {
+//        try
+//        {
+//            string content = File.ReadAllText(file);
+//            Console.WriteLine(content);
+//        }
+//        catch (Exception ex)
+//        {
+//            Console.WriteLine($"Could not read file: {ex.Message}");
+//        }
+//    }
+//}
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
